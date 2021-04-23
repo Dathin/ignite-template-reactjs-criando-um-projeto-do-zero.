7cae633daa7da060040805a7320f57dbed5f1cfa
Pedro Caires <pedro.caires@iteris.com.br>
2021-04-23 00:10:17 -0300

    feat: Header

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -24,9 +24,9 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home() {
+  return null;
+}
 
 // export const getStaticProps = async () => {
 //   // const prismic = getPrismicClient();
